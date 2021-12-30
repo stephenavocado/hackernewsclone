5c6e3c4d573df7ccbd3a0ef50b71e50b96235818
stephenavocado <stephenwalter@chicagobooth.edu>
2021-12-30 14:08:39 +0000

    moved js files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#index'
+  root                  'application#index'
+  get '/new'        =>  'application#new'
+  get '/ask'        =>  'application#ask'
+  get '/show'       =>  'application#show'
+  get '/favorites'  =>  'application#favorites'
 
 end
