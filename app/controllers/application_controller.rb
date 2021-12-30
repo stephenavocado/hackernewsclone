3439d60892353d7a50f5b2a27b529758fda7f559
stephenavocado <stephenwalter@chicagobooth.edu>
2021-12-30 15:22:17 +0000

    HN API connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,36 @@
 class ApplicationController < ActionController::Base
+  before_action :get_stories
+  
   def index
     render({:template => "application/index"})
   end
+
+  def new
+  end
+
+  def ask
+  end
+
+  def show
+  end
+
+  def favorites
+  end
+
+  def get_stories
+    isHomeRoute = request.path == '/'
+    isNewRoute = request.path == '/new'
+    
+    if isHomeRoute 
+      path = '/news'
+    elsif isNewRoute
+      path = 'newest'
+    else
+      path = request.path
+    end 
+      
+    url = "https://node-hnapi.herokuapp.com#{path}"
+    @raw_data = open(url).read
+  end
+
 end
